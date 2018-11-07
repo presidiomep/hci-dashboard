8978b62359e66e591364dcaee74dbb94afe0a976
Max <mxgeller@gmail.com>
2018-11-06 21:38:41 -0800

    Initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-server',
+  templateURL: './server.component.ts'
+});
+export class ServerComponent {
+
+};
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'm-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.scss']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
